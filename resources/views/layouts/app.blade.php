1cfe35856ae87f943c69f922766e4e55787d8b21
a.androsov <alexandr.v.androsov@gmail.com>
2021-04-21 23:57:49 +0300

    upd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
-    @yield('content')
+    <div class="container">
+        @include('includes.header')
+        <div class="row">
+            <div class="col-8">
+                @yield('content')
+            </div>
+            <div class="col-4">
+                @include('includes.aside')
+            </div>
+        </div>
+        @include('includes.footer')
+    </div>
 
-    @include('includes.aside')
 </body>
 </html>
