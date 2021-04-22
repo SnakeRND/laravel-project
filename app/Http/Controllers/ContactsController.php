f7c8dd15416c87dd551c91b877001c3da6cfb1fb
a.androsov <alexandr.v.androsov@gmail.com>
2021-04-22 21:01:35 +0300

    test form errors

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -8,7 +8,10 @@ class ContactsController extends Controller
 {
 
     public function submit(Request $request){
-        dd($request);
+        $validation = $request->validate([
+            'subject' => 'required|min:5|max:50',
+            'message' => 'required|min:15|max:500'
+        ]);
     }
 
 }
