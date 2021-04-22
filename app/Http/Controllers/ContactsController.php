a9aa3033708c6fc9f5ffd22cf12f2a54fdd067fa
a.androsov <alexandr.v.androsov@gmail.com>
2021-04-22 21:07:16 +0300

    fix form errors

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactsRequest;
 
 class ContactsController extends Controller
 {
 
-    public function submit(Request $request){
-        $validation = $request->validate([
-            'subject' => 'required|min:5|max:50',
-            'message' => 'required|min:15|max:500'
-        ]);
+    public function submit(ContactsRequest $request){
+//        $validation = $request->validate([
+//            'subject' => 'required|min:5|max:50',
+//            'message' => 'required|min:15|max:500'
+//        ]);
     }
 
 }
