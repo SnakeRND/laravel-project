e0add65c1b1b6059297db7cd9743efc6619378fd
a.androsov <alexandr.v.androsov@gmail.com>
2021-04-22 20:13:13 +0300

    upd

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,4 +4,31 @@
 
 @section('content')
     <h1>Контакты</h1>
+
+    <form action="{{  route('contact-form')  }}" method="post">
+        @csrf
+
+        <div class="form-group">
+            <label for="name">Введите имя</label>
+            <input type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" placeholder="Введите email" id="email" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="subject">Тема сообщения</label>
+            <input type="text" name="subject" placeholder="Введите текст" id="subject" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="message">Сообщение</label>
+            <textarea name="message" placeholder="Введите сообщение" id="message" class="form-control"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-success">Отправить</button>
+
+    </form>
 @endsection
